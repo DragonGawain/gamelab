9d82cfa5eb771eda605206e1e8fe1254eb771123
allyjenna <allyjenna@me.com>
2024-03-05 21:25:52 -0500

    basic main menu layout

---
+++ Assets/Scripts/GameManagement/GameStateManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStateManager : MonoBehaviour
+{
+    public static GameStateManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    //place scenes in their build order
+    public enum Scene
+    {
+        MainMenu,
+        Credits
+    }
+
+
+    public void LoadScene(Scene scene)
+    {
+        SceneManager.LoadScene(scene.ToString());
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
