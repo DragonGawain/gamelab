dcd2bbb62ef831d38c2d04b5f6b30f74a82046f0
allyjenna <allyjenna@me.com>
2024-03-29 15:33:20 -0400

    Revert "Merge branch 'main' of https://github.com/DragonGawain/gamelab"
    This reverts commit 0c2410a8900862b5d84949951005562b4bba0351, reversing
    changes made to a36d8b46164800c17769f02bf763f7eaba246231.

---
+++ Assets/Scenes/Ally/TestSpawner.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Weapons;
+using static ComboHitBox;
+using static ComboAttackManager;
+
+
+public class TestSpawner : MonoBehaviour
+{ 
+    [SerializeField] private Transform enemyPrefab;
+    [SerializeField] private Transform enemyPrefab2;
+    [SerializeField] private Transform enemyPrefab3;
+    //[SerializeField] private Transform enemyPrefab4;
+    //[SerializeField] private Transform enemyPrefab5;
+    [SerializeField] WeaponType weaponType;
+    public LevelManager level;
+    [SerializeField] private int health = 100;
+
+
+
+    private void Start()
+    {
+   
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SpawnEnemy(enemyPrefab);
+            Debug.Log("enemy tag: " + enemyPrefab.tag);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SpawnEnemy(enemyPrefab2);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SpawnEnemy(enemyPrefab3);
+        }
+        //else if (Input.GetKeyDown(KeyCode.Alpha4))
+        //{
+        //    SpawnEnemy(enemyPrefab4);
+        //}
+        //else if (Input.GetKeyDown(KeyCode.Alpha5))
+        //{
+        //    SpawnEnemy(enemyPrefab5);
+        //}
+    }
+
+    void SpawnEnemy(Transform enemyPrefab)
+    {
+        // Instantiate the enemy directly above the spawn point
+        Instantiate(enemyPrefab, transform.position + Vector3.up, Quaternion.identity);
+    }
+
+
+    private void HitByBlaster(Blaster blaster)
+    {
+        // deals blaster damage to health
+        health -= blaster.GetDamage();
+
+        // any other blaster effects can go here
+
+    }
+
+    private void HitByHammer(Hammer hammer)
+    {
+        // deals blaster damage to health
+        health -= hammer.GetDamage();
+
+        // any other blaster effects can go here
+
+    }
+
+    private void HitByFlamethrower(Flamethrower thrower)
+    {
+        // deals blaster damage to health
+        health -= thrower.GetDamage();
+
+        // any other blaster effects can go here
+
+    }
+
+    private void HitByGrenadeLauncher(GrenadeLauncher launcher)
+    {
+        // deals blaster damage to health
+        health -= launcher.GetDamage();
+
+        // any other blaster effects can go here
+
+    }
+
+ 
+
+
+}
