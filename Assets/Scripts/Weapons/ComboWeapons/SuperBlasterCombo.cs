75c8b257d95f4259a1049ad6d3586f136c4bb8d0
allyjenna <allyjenna@me.com>
2024-04-06 13:30:22 -0400

    weird bug

--- Assets/Scripts/Weapons/ComboWeapons/SuperBlasterCombo.cs
+++ Assets/Scripts/Weapons/ComboWeapons/SuperBlasterCombo.cs
@@ -1,16 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using Weapons;
 
-namespace Scenes.Ally
+namespace Weapons
 {
     public class SuperBlasterCombo : MonoBehaviour
     {
-        private void OnTriggerStay(Collider other)
+        private void OnTriggerEnter(Collider other)
         {
+
+        
+            Debug.Log("Ally is touching " + other.name);
+
             if (other.gameObject.CompareTag("Hammer"))
             {
+                Debug.Log("Ally is inside things");            
                 // Debug.Log("here!!!!!!!!!: " + other.gameObject + " and " + this.gameObject);
+
                 ComboAttackManager.SpawnSuperBlaster();
+            
+
             }
+
         }
     }
 }
