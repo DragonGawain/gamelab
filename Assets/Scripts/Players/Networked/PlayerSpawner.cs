296daf38d28a938c796d52dfb17d7849756a6d43
Sarah01312 <sarahmu333@hotmail.com>
2024-04-07 01:45:39 -0400

    networking 2 am insanity

--- Assets/Scripts/Players/Networked/PlayerSpawner.cs
+++ Assets/Scripts/Players/Networked/PlayerSpawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
@@ -8,14 +9,35 @@ public class PlayerSpawner : NetworkBehaviour
     [SerializeField] private GameObject playerPrefabA; //add prefab in inspector
     [SerializeField] private GameObject playerPrefabB; //add prefab in inspector
 
+    [SerializeField] private NetworkPrefabsList _networkPrefabsList;
+    private int choice = 0;
+
+
+    public override void OnNetworkSpawn()
+    {
+        SpawnPlayerServerRpc(OwnerClientId, choice);
+        choice++;
+    }
+
     [ServerRpc(RequireOwnership = false)] //server owns this object but client can request a spawn
     public void SpawnPlayerServerRpc(ulong clientId, int prefabId)
     {
+        Debug.Log(choice);
+        prefabId = choice;
+        _networkPrefabsList.PrefabList.Equals(prefabId);
         GameObject newPlayer;
+
         if (prefabId == 0)
-            newPlayer = (GameObject)Instantiate(playerPrefabA);
+        {
+            //choice++;
+            Debug.Log("dark player");
+            newPlayer = Instantiate(playerPrefabA);
+        }
         else
-            newPlayer = (GameObject)Instantiate(playerPrefabB);
+        {
+            Debug.Log("light player");
+            newPlayer = Instantiate(playerPrefabB);
+        }
 
         NetworkObject netObj = newPlayer.GetComponent<NetworkObject>();
         newPlayer.SetActive(true);
