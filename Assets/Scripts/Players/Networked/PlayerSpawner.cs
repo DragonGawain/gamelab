3a43921efb11bf3993ba0f2d0c177d9bd72aa7fc
Zaid Minhas <61346884+ZaidMinhas@users.noreply.github.com>
2024-04-15 13:47:16 -0400

    Small push

--- Assets/Scripts/Players/Networked/PlayerSpawner.cs
+++ Assets/Scripts/Players/Networked/PlayerSpawner.cs
@@ -27,30 +27,29 @@ public class PlayerSpawner : NetworkBehaviour
         
         // REMOVE COMMENTS AFTER UI to GAME SCENE IS FIXED
         
-         hostOption = SelectPlayer.hostSelection;
-        
-         if (hostOption == 0) { clientOption = 1; }
-         else { clientOption = 0; }
-        
-         if(SelectPlayer.confirm)
-         {
-             Debug.Log("Spawning players...");
-             SpawnPlayerServerRpc(0, hostOption);
-             SpawnPlayerServerRpc(1, clientOption);
-
-             SelectPlayer.confirm = false;
-         }
+         // hostOption = SelectPlayer.hostSelection;
+         //
+         // if (hostOption == 0) { clientOption = 1; }
+         // else { clientOption = 0; }
+         //
+         // if(SelectPlayer.confirm)
+         // {
+         //     Debug.Log("Spawning players...");
+         //     SpawnPlayerServerRpc(0, hostOption);
+         //     SpawnPlayerServerRpc(1, clientOption);
+         //     SelectPlayer.confirm = false;
+         // }
 
         //For Zaid to test weapon stuff
         //DELETE CODE AFTER UI IS FIXED
-        /*if (Input.GetKeyDown(KeyCode.X))
+        if (Input.GetKeyDown(KeyCode.X))
         {
             SpawnPlayerServerRpc(0, 0);
             SpawnPlayerServerRpc(1, 1);
             PlayerSpawn();
             if (TM!=null){TM.enabled = true;}
             
-        }*/
+        }
     }
 
     public override void OnNetworkSpawn()
