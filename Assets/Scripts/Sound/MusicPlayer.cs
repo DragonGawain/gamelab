7461d1b54fd4e74eb3dd7b08b6a3ac3393fff41b
Craig <craigk00@hotmail.com>
2024-04-15 23:06:01 -0400

    MAGIC!

--- Assets/Scripts/Sound/MusicPlayer.cs
+++ Assets/Scripts/Sound/MusicPlayer.cs
@@ -4,42 +4,29 @@ using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
 {
-
     // public ParticleSystem crystalParticles1; // Reference to the Particle System component
     // public ParticleSystem crystalParticles2; // Reference to the Particle System component
 
     public AudioSource musicAudioSource; // Reference to the AudioSource component for playing music
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    ParticleSystem lightParticles = null;
+    ParticleSystem darkParticles = null;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("LightPlayer")) // Check if the collider belongs to the player
         {
-            other.gameObject.GetComponentInChildren<ParticleSystem>().Play();
-            // crystalParticles1.Play(); // Play the particle system
+            lightParticles = other.gameObject.GetComponentInChildren<ParticleSystem>();
+            lightParticles.Play();
 
             musicAudioSource.Play(); // Play the music
-
         }
         if (other.CompareTag("DarkPlayer")) // Check if the collider belongs to the player
         {
-            other.gameObject.GetComponentInChildren<ParticleSystem>().Play();
-            // crystalParticles2.Play(); // Play the particle system
+            darkParticles = other.gameObject.GetComponentInChildren<ParticleSystem>();
+            darkParticles.Play();
 
             musicAudioSource.Play(); // Play the music
-
         }
     }
 
@@ -47,17 +34,27 @@ public class MusicPlayer : MonoBehaviour
     {
         if (other.CompareTag("LightPlayer")) // Check if the collider belongs to the player
         {
-            other.gameObject.GetComponentInChildren<ParticleSystem>().Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
-            // crystalParticles1.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear); // Stop the particle system immediately and clear existing particles
+            other.gameObject
+                .GetComponentInChildren<ParticleSystem>()
+                .Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+            lightParticles = null;
             musicAudioSource.Stop(); // Play the music
-
         }
         if (other.CompareTag("DarkPlayer")) // Check if the collider belongs to the player
         {
-            other.gameObject.GetComponentInChildren<ParticleSystem>().Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
-            // crystalParticles2.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear); // Stop the particle system immediately and clear existing particles
+            other.gameObject
+                .GetComponentInChildren<ParticleSystem>()
+                .Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+            darkParticles = null;
             musicAudioSource.Stop(); // Play the music
-
         }
+    }
+
+    private void OnDestroy()
+    {
+        if (lightParticles != null)
+            lightParticles.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        if (darkParticles != null)
+            darkParticles.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
     }
 }
