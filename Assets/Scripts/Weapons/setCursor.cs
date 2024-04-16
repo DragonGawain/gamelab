8643be66e3408608f82e066c6608f5b3e31d1028
Kira <kafountain5@gmail.com>
2024-04-16 03:03:38 -0400

    custom cursor

--- Assets/Scripts/Weapons/setCursor.cs
+++ Assets/Scripts/Weapons/setCursor.cs
@@ -6,11 +6,31 @@ public class setCursor : MonoBehaviour
 {
 
     public Texture2D crosshair;
+    public Texture2D mouseCursor;
+    private Vector2 cursorOffset;
+    
     // Start is called before the first frame update
     void Start()
     {
+        enableGameCursor();
+    }
+
+    private void settingNewCursor()
+    {
         // center the cursor's origin cause by default its in the top left corner
         Vector2 cursorOffset = new Vector2(crosshair.width / 2, crosshair.height / 2);
+    }
+    public void enableGameCursor()
+    {
+        
+        settingNewCursor();
+        Cursor.SetCursor(crosshair, cursorOffset, CursorMode.Auto);
+        
+    }
+
+    public void enableUICursor()
+    {
+        settingNewCursor();
         
         Cursor.SetCursor(crosshair, cursorOffset, CursorMode.Auto);
     }
