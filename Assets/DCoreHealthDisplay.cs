6546d22740ae847bd682d4c950eda81a7bd819b2
Zaid Minhas <61346884+ZaidMinhas@users.noreply.github.com>
2024-04-17 02:42:26 -0400

    AAAAAAAAAAAAAAAAAAAAAAAAAA

--- Assets/DCoreHealthDisplay.cs
+++ Assets/DCoreHealthDisplay.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using Unity.Netcode;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DCoreHealthDisplay : NetworkBehaviour
@@ -10,11 +11,24 @@ public class DCoreHealthDisplay : NetworkBehaviour
     [SerializeField] private TextMeshProUGUI[] dCoresTMP;
     
     [SerializeField] private DCore[] dCores;
+
+    void Start()
+    {
+        Debug.Log("GAMMA RAYS");
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        Debug.Log("Ligma balls");
+    }
+
     // Start is called before the first frame update
     void Update()
     {
-        if (!IsSpawned && !IsServer) return;
         
+        if (!IsSpawned && !IsServer) return;
+        print("YOU SPAWNED, DUMBASS");
         for (int i = 0; i < 4; i++)
         {
             if (dCores[i] != null)
